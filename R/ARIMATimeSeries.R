99eeb1e22d9167fa12510f3af7347736e7f83951
František Bartoš <38475991+FBartos@users.noreply.github.com>
2025-06-27 21:17:10 +0200

    Update R/ARIMATimeSeries.R

--- R/ARIMATimeSeries.R
+++ R/ARIMATimeSeries.R
@@ -451,7 +451,6 @@ ARIMATimeSeries <- function(jaspResults, dataset, options) {
           )
         )
       }
-      # covariatesForecast <- covariates[rangeForecast]
       covariatesForecast <- covariates[rangeForecast, ]
       xreg <- as.matrix(covariatesForecast)
     }
